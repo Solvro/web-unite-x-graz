6936807c3c2050ae5d5882d70f416cb8e7a947fd
zeolsem <zsolim@proton.me>
2025-06-09 18:20:45 +0200

    feat: main layout

---
+++ src/app/journey/page.tsx
@@ -0,0 +1,8 @@
+export default function JourneyPage() {
+  return (
+    <div className="flex flex-col items-center justify-between">
+      <h1 className="mt-32 text-3xl font-bold">Journey Page</h1>
+      <p className="mt-4 text-lg">This is the journey page content.</p>
+    </div>
+  );
+}
---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+import { vt323 } from "@/lib/fonts";
+
+export function Footer() {
+  return (
+    <footer className="mt-auto w-full bg-[#101314] p-4 py-8 text-center">
+      <p className={`${vt323.className} text-md`}>
+        Made with 💚️ by{" "}
+        <Link href="https://solvro.pwr.edu.pl/pl/" className="underline">
+          Solvro
+        </Link>
+      </p>
+    </footer>
+  );
+}
