d239db8c46d2ecb7838374574ae8b9a1cb95a260
ShadowCatP <282276@student.pwr.edu.pl>
2025-06-10 08:38:19 +0200

    feat: initialize basic 3d

---
+++ src/hooks/use-scroll-section.ts
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface useScrollSectionOptions {
+  maxSections?: number;
+  sectionHeight?: number; // in viewport units, e.g.: 50vh = 0.5
+}
+
+export const useScrollSection = ({
+  maxSections = 1,
+  sectionHeight = 0.5,
+}: useScrollSectionOptions) => {
+  const [currentSection, setCurrentSection] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const windowHeight = window.innerHeight;
+      const section = Math.floor(scrollY / (windowHeight * sectionHeight));
+      setCurrentSection(Math.min(section, maxSections - 1));
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [maxSections, sectionHeight]);
+
+  return currentSection;
+};
