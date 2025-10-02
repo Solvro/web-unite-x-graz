fd3baadcf88ebded83008381d581fad3692ca8d8
Tymon Jędryczka <jedryczkatymon@gmail.com>
2025-10-02 18:10:17 +0200

    fix: eslint issues

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,11 @@ import reactThree from "@react-three/eslint-plugin";
 import { solvro } from "@solvro/config/eslint";
 
 export default solvro({
-  files: ["**/*.{js,ts,jsx,tsx}"],
+  files: ["**/*.{js,mjs,ts,jsx,tsx}"],
   plugins: { "@react-three": reactThree },
+  //eslint-disable-next-line import/no-named-as-default-member
   rules: {
     ...reactThree.configs.recommended.rules,
-    "react/no-unknown-property": "off", // conflict with properties of react fiber
+    "react/no-unknown-property": "off",
   },
 });
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,3 @@
-const config = {
+export const config = {
   plugins: ["@tailwindcss/postcss"],
 };
-
-export default config;
