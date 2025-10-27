67d9a17c92ab74ad7273999fe3f3b860c3e9edca
Bohdan Koshkin <153859662+ShadowCatP@users.noreply.github.com>
2025-10-27 16:17:36 +0100

    Feature/3d presentation issue 7 (#38)
    * feat: initialize basic 3d
    
    * refactor: folder structure refactor/changed to feature based structure
    
    * refactor: removed unnecessary 3d-background component
    
    * feat: synchronize models to html
    
    * feat: added html cards
    
    * feat: added scrolling behavior and animations
    
    * fix: improved performance
    
    * refactor: scroll triggered animations
    
    * feat: custom animation timeline support
    
    * refactor: added some basic animations
    
    * fix: models flashing for 1 frame on page load
    
    * refactor: slight adjustments to animations/cards
    
    * refactor: moved sections declaration to the separate file
    
    * fix: lint
    
    * feat: added smooth scroll
    
    * feat: added text reveal animation
    
    * refactor: made section card fill more screen
    
    * refactor: reworked scroll animations, added loading screen, mock model loading
    
    * fix: ci requirements
    
    * fix: ci requirements 2.0
    
    * fix: lint rules breaking
    
    * fix: linting issues 2
    
    * feat: added 3d models to the public folder
    
    * chore: audit fix
    
    ---------
    
    Co-authored-by: zeolsem <zsolim@proton.me>

--- src/features/3d-presentation/components/section-card.tsx
+++ src/features/3d-presentation/components/section-card.tsx
@@ -1,15 +1,47 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect, useRef } from "react";
 import type { ReactNode } from "react";
 
 interface SectionCardProps {
   id: string;
   children: ReactNode;
+  className?: string;
 }
 
-export function SectionCard({ id, children }: SectionCardProps) {
+export function SectionCard({
+  id,
+  children,
+  className,
+  ...props
+}: SectionCardProps) {
+  const sectionRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const section = sectionRef.current;
+
+    if (section === null) {
+      return;
+    }
+
+    gsap.set(section, { opacity: 0 });
+
+    ScrollTrigger.create({
+      trigger: section,
+      start: "top center",
+      end: "center center",
+      onEnter: () => {
+        gsap.fromTo(section, { opacity: 0, y: 150 }, { opacity: 1, y: 0 });
+      },
+    });
+  }, []);
+
   return (
-    <section id={id} className="h-[300vh]">
-      <div className="sticky top-0 flex h-screen w-full items-center justify-center">
-        <div className="grid w-full max-w-7xl grid-cols-2 gap-12 border border-white p-4">
+    <section ref={sectionRef} id={id} className="h-[300vh]" {...props}>
+      <div className="sticky top-0 flex h-screen w-full items-center justify-center px-8 py-32">
+        <div
+          className={`grid h-full w-full grid-cols-2 gap-12 border border-white p-4 ${String(className)}`}
+        >
           {children}
         </div>
       </div>
--- src/features/3d-presentation/components/ui.tsx
+++ src/features/3d-presentation/components/ui.tsx
@@ -1,27 +1,14 @@
-/* eslint-disable react/no-unescaped-entities */
-import { Box, Cone } from "@react-three/drei";
-import type { ReactNode } from "react";
-import type * as THREE from "three";
+"use client";
 
-import { SlideInOutRotateScale } from "@/lib/animations";
-import { degToRad } from "@/lib/utils";
+import { SectionCard } from "./section-card";
 
-interface Section {
-  id: string;
-  content: ReactNode;
-  model: ReactNode;
-  modelLeft?: boolean;
-  animation?: (ref: THREE.Group) => gsap.core.Timeline;
-}
-
-export const journeySections: Section[] = [
-  {
-    id: "section1",
-    content: (
-      <>
+export function UI() {
+  return (
+    <>
+      <SectionCard id={"section1"}>
         <h2 className="text-5xl font-bold">Purifying the Sand</h2>
         <p className="col-start-2 row-start-2 text-right">
-          Common sand contains a lot of silicon dioxide (SiO₂), but it's not
+          Common sand contains a lot of silicon dioxide (SiO₂), but it is not
           pure enough for electronics. The journey begins by extracting and
           refining silicon from quartz sand. Through a high-temperature chemical
           process, silicon is isolated and purified to 99.9999% purity—a
@@ -30,22 +17,8 @@ export const journeySections: Section[] = [
         <p className="row-start-3 mt-64 text-neutral-500">
           Silicon is isolated and purified to 99.9999% purity.
         </p>
-      </>
-    ),
-    model: (
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial color={"#ffffff"} wireframe />
-      </mesh>
-    ),
-    modelLeft: true,
-    animation: (group: THREE.Group) =>
-      SlideInOutRotateScale(group, "#section1", 1.6, { x: degToRad(20) }, true),
-  },
-  {
-    id: "section2",
-    content: (
-      <>
+      </SectionCard>
+      <SectionCard id={"section2"} className="reveal-type">
         <h2 className="col-start-2 text-5xl font-bold">
           Growing a Silicon Ingot
         </h2>
@@ -55,30 +28,22 @@ export const journeySections: Section[] = [
           into a cylindrical ingot—a perfect lattice structure that forms the
           base for every modern microchip.
         </p>
-      </>
-    ),
-    model: <Box />,
-  },
-  {
-    id: "section3",
-    content: (
-      <>
+      </SectionCard>
+      <SectionCard id={"section3"} className="reveal-type">
         <h2 className="text-5xl font-bold">Wafer Slicing and Polishing</h2>
         <p className="col-start-2 row-start-2 mb-40 text-right">
           The silicon ingot is sliced into ultra-thin wafers, like microscopic
-          pancakes. Each wafer is polished until it's mirror-smooth, creating a
+          pancakes. Each wafer is polished until it is mirror-smooth, creating a
           flat surface ready for circuit printing. These wafers are now the
           canvas for microchip design.
         </p>
         <p className="col-span-2 row-start-3 mt-24 mb-5">
           The silicon ingot is sliced into ultra-thin wafers, like microscopic
-          pancakes. Each wafer is polished until it's mirror-smooth, creating a
+          pancakes. Each wafer is polished until it is mirror-smooth, creating a
           flat surface ready for circuit printing. These wafers are now the
           canvas for microchip design.
         </p>
-      </>
-    ),
-    model: <Cone />,
-    modelLeft: true,
-  },
-];
+      </SectionCard>
+    </>
+  );
+}
